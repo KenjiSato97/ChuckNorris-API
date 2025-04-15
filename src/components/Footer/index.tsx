6b56c3d139c372caaa96df83afd50ea05ab636fc
Kenji Aranha Sato <KenjiSato@academicos.com>
2025-04-14 21:04:32 -0300

    feat: estrutura do projeto

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import "./src/components/Footer/styles.css";
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-content">
+                <img src="./footer-gif.gif" alt="Icon" className="footer-icon" />
+                <p>&#169; Create by Álekiss Melo - TFA - 2024.2</p>
+            </div>
+        </footer>
+    );
+};
+export default Footer;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import "./styles.css"
+const Header = () => {
+return (
+<div className="header">
+<img src="./logo.png" />
+</div>
+)
+}
+export default Header
